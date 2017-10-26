23d4f122c916640a4f0ebefd77073a9ae3eb4507
yorubawebtuts <azeez@yorubawebtuts.com.ng>
2017-10-26 22:30:10 +0100

    init commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+ $method = $_SERVER['REQUEST_URI'];
+
+ if( $method == 'POST') {
+     $responseBody = file_get_contents('php://input');
+     $json = json_decode($responseBody);
+     $text = $json->result->parameters->text;
+     
+     switch ($text) {
+        case 'hi':
+            $speech = "Hi, Nice to meet you";
+            break;
+        case 'bye':
+            $speech = "Bye, good night";
+            break;
+        default:
+            break;
+     }
+     $response = new \stdClass();
+     $response->speech = "";
+     $response->displayText = "";
+     $response->source = "webhook";
+     echo json_encode($response);
+ } else {
+     echo 'Method Not Allowed !';
+ }
